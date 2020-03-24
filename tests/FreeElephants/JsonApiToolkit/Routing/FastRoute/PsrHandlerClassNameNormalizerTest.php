8ff1006d6cfc9bda2e85fef4a30acef1be7d66aa
n.gnato <nikolay.gnato@sshvps.net>
2020-03-24 12:29:24 +0300

    Fix normalizer class name

--- tests/FreeElephants/JsonApiToolkit/Routing/FastRoute/PsrHandlerClassNameNormalizerTest.php
+++ tests/FreeElephants/JsonApiToolkit/Routing/FastRoute/PsrHandlerClassNameNormalizerTest.php
@@ -7,7 +7,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-class OperationHandlerPsrNormalizerTest extends AbstractTestCase
+class PsrHandlerClassNameNormalizerTest extends AbstractTestCase
 {
 
     public function testNormalizeRequestHandlerCallable()
